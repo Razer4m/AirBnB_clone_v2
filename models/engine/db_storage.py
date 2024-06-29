4bb5da1cd68006862c02f1eb2cb0d51c65fdc2a3
Razer4m <tumelomorakile@gmail.com>
2024-06-29 16:01:40 +0200

    Updates

---
+++ models/engine/db_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" DBStorage Module for HBNB project """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import os
+
+
+class DBStorage:
+    """ Database Storage class """
+
+    __engine = None  # Private class attribute: set to None
+    __session = None  # Private class attribute: set to None
+
+    def __init__(self):
+        """ Initialize DBStorage """
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST', default='localhost')
+        db = os.getenv('HBNB_MYSQL_DB')
+        env = os.getenv('HBNB_ENV')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(user, pwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Query all objects depending on the class name """
+        classes = [User, State, City, Amenity, Place, Review]
+        objects_dict = {}
+
+        if cls:
+            objects = self.__session.query(cls).all()
+        else:
+            objects = []
+            for cls in classes:
+                objects.extend(self.__session.query(cls).all())
+
+        for obj in objects:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            objects_dict[key] = obj
+
+        return objects_dict
+
+    def new(self, obj):
+        """ Add the object to the current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Delete from the current database session obj if not None """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Create all tables in the database and create a session """
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'  # Table name in the database
+
+    name = Column(String(128), nullable=False)  # State name column
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref=backref("state", cascade="all"),
+                              passive_deletes=True)
+    else:
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            city_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
